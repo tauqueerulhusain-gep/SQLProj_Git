31bfd5d6ba3a9122e8019abf5466c46fc0cb57cc
Tauqueerul Husain Momin <Tauqueerul.Momin@gep.com>
2019-12-02 16:06:32 +0530

    initial structure.

---
+++ SQLProj_Git/dbo/Tables/Department.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Department] (
+    [Id]       INT           NOT NULL,
+    [DeptName] NVARCHAR (50) NULL,
+    [ManagerId] INT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ SQLProj_Git/dbo/Tables/Employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]      INT           NULL,
+    [Name]    NVARCHAR (50) NULL,
+    [Address] NVARCHAR (50) NULL,
+    [DeptId]  INT           NULL
+);
+
